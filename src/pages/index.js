58c313586b37c555ef1cf834932d2d81b317bce0
Abhishek P <abhishek.p@piktorlabs.com>
2021-08-13 11:48:36 +0530

    Added SEO to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
 import BannerSection from '../templates/BannerSection';
 import EditorsChoiceSection from '../organisms/EditorsChoice';
@@ -13,36 +12,14 @@ import SEO from '../atoms/SEO';
 
 const HomePage = () => (
   <>
-    <Helmet>
-      <title>The Intersectional Feminist</title>
-      <meta
-        name="description"
-        content="The If Mag The Best Blogs on every topic"
-      />
-      <script type="application/ld+json">
-        {`
-        {
-          "@context": "https://schema.org",
-          "@type": "Organization",
-          "url": "https://www.theifmag.com",
-          "name": "The Intersectional Feminist",
-          "contactPoint": {
-            "@type": "ContactPoint",
-            "email": "theifmag@gmail.com",
-            "contactType": "The Team"
-          }
-        }
-      `}
-      </script>
-      <SEO
-        title="The Intersectional Feminist"
-        keywords={['feminism, intersectional']}
-        author="The Intersectional Feminist"
-        siteUrl="https://www.theifmag.com"
-        // image={{src: siteUrl + thumbnail}}
-        // description={excerpt}
-      />
-    </Helmet>
+    <SEO
+      title="The Intersectional Feminist"
+      keywords={['feminism, intersectional']}
+      author="The Intersectional Feminist"
+      siteUrl="https://www.theifmag.com"
+      // image={{src: siteUrl + thumbnail}}
+      // description={excerpt}
+    />
     <BannerSection />
     <EditorsChoiceSection />
     <WhoWeAre />
